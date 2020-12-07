ace0f845713282f23a443954a1f238d983b032e4
makn <1055482519@qq.com>
2020-12-07 21:37:39 +0800

    增加读取文件数据功能

---
+++ src/main/java/cn/makn/file/util/BufferedFileUtils.java
@@ -0,0 +1,55 @@
+package cn.makn.file.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+/**
+ * @Description:
+ * @author: makn
+ * @version: V1.0
+ * @date: 2020/12/7 21:13
+ */
+public class BufferedFileUtils extends RandomAccessFile {
+    private static final String [] model = {"r","rw","rws","rwd"};
+
+    /**
+     * @param name
+     * @param mode r	以只读的方式打开文本，也就意味着不能用write来操作文件
+     *             rw	读操作和写操作都是允许的
+     *             rws	每当进行写操作，同步的刷新到磁盘，刷新内容和元数据
+     *             rwd	每当进行写操作，同步的刷新到磁盘，刷新内容
+     * @return
+     * @Description:
+     * @author makn
+     * @date 2020/12/7 15:20
+     */
+    public BufferedFileUtils(String name, String mode) throws FileNotFoundException {
+        super(name, mode);
+    }
+
+    public BufferedFileUtils(File file, String mode) throws FileNotFoundException {
+        super(file, mode);
+    }
+
+    public static RandomAccessFile getRAFWithModelR(String filePath) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(new File(filePath), model[0]);
+        return raf;
+    }
+
+    public static RandomAccessFile getRAFWithModelRW(String filePath) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(new File(filePath), model[1]);
+        return raf;
+    }
+
+    public static RandomAccessFile getRAFWithModelRWS(String filePath) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(new File(filePath), model[2]);
+        return raf;
+    }
+
+    public static RandomAccessFile getRAFWithModelRWD(String filePath) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(new File(filePath), model[3]);
+        return raf;
+    }
+
+}
--- src/main/java/cn/makn/file/util/FileUtils.java
+++ src/main/java/cn/makn/file/util/FileUtils.java
@@ -1,6 +1,10 @@
 package cn.makn.file.util;
 
+import cn.makn.file.model.FileDate;
+
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @Description:
@@ -50,5 +54,39 @@ public class FileUtils {
         }
 
         return count;
+    }
+
+    /**
+     * @Description: 获取文件数据
+     * @author makn
+     * @date 2020/12/7 21:21
+     * @param filePath 文件路径 + 文件名称
+     * @param num 读取条数
+     * @return
+     */
+    public static FileDate getFileDate(String filePath, int num) throws IOException {
+        FileDate fileDate = new FileDate();
+        List<String> bodyDate = new LinkedList<String>();
+        // 以只读的方式打开文本
+        RandomAccessFile raf = BufferedFileUtils.getRAFWithModelR(filePath);
+        // 每行数据
+        String tempString = null;
+        // 行数标
+        int line = 0;
+        // 小于行数时读取数据
+        while (line < num) {
+            // 读取下一行数据
+            tempString = raf.readLine();
+            // 不为空加入body中
+            if(tempString != null){
+                bodyDate.add(tempString);
+                line++;
+            }
+        }
+        // 关闭流
+        raf.close();
+        fileDate.setBodyDate(bodyDate);
+
+        return fileDate;
     }
 }
