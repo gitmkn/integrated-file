55d4410094cea73aa99bcfe5fbcb3e59d9f58d6f
makn <1055482519@qq.com>
2020-12-12 16:53:16 +0800

    处理getFileDate参数num上送-1bug

--- src/main/java/cn/makn/file/util/FileUtils.java
+++ src/main/java/cn/makn/file/util/FileUtils.java
@@ -17,11 +17,11 @@ import java.util.Map;
 public class FileUtils {
 
     /**
+     * @param filePath 文件路径 +文件名称
+     * @return
      * @Description: 获取文件总行数（只获取文件行数）
      * @author makn
      * @date 2020/12/7 19:52
-     * @param filePath 文件路径 +文件名称
-     * @return
      */
     public static int getRowCount(String filePath) {
         int count = 0;
@@ -36,8 +36,8 @@ public class FileUtils {
         } catch (IOException e) {
             count = -1;
             e.printStackTrace();
-        }finally {
-            if(bufferedReader != null){
+        } finally {
+            if (bufferedReader != null) {
                 try {
                     bufferedReader.close();
                 } catch (IOException e) {
@@ -45,10 +45,10 @@ public class FileUtils {
                     e.printStackTrace();
                 }
             }
-            if(inputStream != null){
+            if (inputStream != null) {
                 try {
                     inputStream.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     count = -1;
                     e.printStackTrace();
                 }
@@ -59,13 +59,13 @@ public class FileUtils {
     }
 
     /**
+     * @param filePath 文件路径 + 文件名称
+     * @param num      段行数
+     * @param size     缓冲
+     * @return
      * @Description: 获取文件从条数，及段落偏移量
      * @author makn
      * @date 2020/12/8 15:39
-     * @param filePath 文件路径 + 文件名称
-     * @param num 段行数
-     * @param size 缓冲
-     * @return
      */
     public static Map<String, Object> getRowCount(String filePath, int num, int size) throws IOException {
         // 单行偏移量
@@ -89,7 +89,7 @@ public class FileUtils {
             fileParagraphs.add(new FileParagraph(total, pos));
 
             // 读取无结果或不满足段数（最后一段），退出读取
-            if(total == 0 || total < num){
+            if (total == 0 || total < num) {
                 break;
             }
             // 偏移量
@@ -104,18 +104,18 @@ public class FileUtils {
     }
 
     /**
+     * @param filePath 文件路径 + 文件名称
+     * @param pos      偏移量
+     * @param num      读取条数 -1:读取所有(慎用，要防止内存溢出)
+     * @param size     缓冲 -1默认缓冲
+     * @return List<String>
      * @Description: 获取文件数据
      * @author makn
      * @date 2020/12/7 21:21
-     * @param filePath 文件路径 + 文件名称
-     * @param pos 偏移量
-     * @param num 读取条数 -1:读取所有
-     * @param size 缓冲
-     * @return List<String>
      */
-    public static List<String> getFileDate(String filePath, long pos, int num, int size) throws IOException {
+    public static List<String> getFileDate(String filePath, String encoding, long pos, int num, int size) throws IOException {
         List<String> date = new LinkedList<String>();
-        if(size < 0){
+        if (size < 0) {
             size = -1;
         }
         // 读取R
@@ -123,9 +123,15 @@ public class FileUtils {
         // 偏移段落
         raf.seek(pos);
         int count = 0;
+        String strLine;
         while (count < num || num == -1) {
-            date.add(raf.readLine());
-            count++;
+            strLine = raf.readLine();
+            if (strLine != null) {
+                date.add(new String(strLine.getBytes("8859_1"), encoding));
+                count++;
+            } else {
+                break;
+            }
         }
         // 关闭流
         raf.close();
@@ -135,17 +141,17 @@ public class FileUtils {
     }
 
     /**
+     * @param file 文件
+     * @param num  段行数
+     * @param pos  偏移量
+     * @param size 缓冲
+     * @return FileParagraph 当前段落行数及偏移量
      * @Description: 根据偏移量读取下一段落（外部不可用）
      * @author makn
      * @date 2020/12/8 15:41
-     * @param file 文件
-     * @param num 段行数
-     * @param pos 偏移量
-     * @param size 缓冲
-     * @return FileParagraph 当前段落行数及偏移量
      */
     private static FileParagraph getRowCountAndPos(File file, int num, long pos, int size) throws IOException {
-        if(size < 0){
+        if (size < 0) {
             size = -1;
         }
         // 读取R
